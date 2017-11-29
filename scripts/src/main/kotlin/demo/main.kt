c3ca5d84b3fec1ae200672259a61e4f8e4bd7812
Victor Oliveira <victorlaertedoliveira@gmail.com>
2017-11-28 21:46:11 -0300

    Remove stop words and one-letter words

--- scripts/src/main/kotlin/demo/main.kt
+++ scripts/src/main/kotlin/demo/main.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>) {
 //  removeHtml()
 //  removeURLs()
 //	removePunctuation()
+	removeStopWordsAndOneLetterWords()
 }
 
 fun getReadFile(fileName: String): File = File(fileName)
@@ -54,7 +55,6 @@ fun removeURLs() {
 }
 
 fun removePunctuation() {
-
 	val readFile = getReadFile("kotlin-posts-3.csv")
 
 	if (readFile.exists()) {
@@ -64,6 +64,31 @@ fun removePunctuation() {
 		lines.forEach {
 			var text = it.replace(regex, " ")
 			newFile.appendText("$text\n")
+		}
+	}
+}
+
+fun removeStopWordsAndOneLetterWords() {
+	val readFile = getReadFile("kotlin-posts-4.csv")
+
+	if (readFile.exists()) {
+		val lines = Files.lines(Paths.get(readFile.toURI()))
+		val newFile = getOrCreateWriteFile("kotlin-posts-5.csv")
+		val stopWords = listOf("a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours	ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves")
+
+		lines.forEach {
+			var text = it.toLowerCase()
+			var words = text.split(" ").toMutableList()
+			var newLine = StringBuffer()
+
+			words.forEach {
+				if (!stopWords.contains(it) && it.length > 1) {
+					newLine.append(it)
+					newLine.append(" ")
+				}
+			}
+
+			newFile.appendText("$newLine\n")
 		}
 	}
 }
