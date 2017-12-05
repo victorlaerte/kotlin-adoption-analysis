4e71aaf3a3f0b4cecd530fb9154c5aaa6c42ea36
Victor Oliveira <victorlaertedoliveira@gmail.com>
2017-12-04 21:38:23 -0300

    Run Porter Stemmer Algorithm

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 - **[kotlin-posts-2.csv](/kotlin-posts-2.csv) - Remove html tags - DONE**
 - **[kotlin-posts-3.csv](/kotlin-posts-3.csv) - Remove URL's - DONE**
 - **[kotlin-posts-4.csv](/kotlin-posts-4.csv) - Remove punctuation - DONE**
-- **[kotlin-posts-5.csv](/kotlin-posts-5.csv) - Remove stop words - DONE**
-- **[kotlin-posts-6.csv](/kotlin-posts-6.csv) - Remove one-letter words - DONE**
-- [kotlin-posts-7.csv](/kotlin-posts-7.csv) - Apply Porter Stemming Algorithm - [Porter Stemming Repo](https://github.com/victorlaerte/java-porter-stemming)
+- **[kotlin-posts-5.csv](/kotlin-posts-5.csv) - Remove stop words and one-letter words- DONE**
+- **[kotlin-posts-6.csv](/kotlin-posts-6.csv) - Apply Porter Stemming Algorithm - [Porter Stemming Repo](https://github.com/victorlaerte/java-porter-stemming) - DONE**
 - [results.txt](/results.txt) - Apply LDA algorithm
--- scripts/src/main/kotlin/demo/main.kt
+++ scripts/src/main/kotlin/demo/main.kt
@@ -9,7 +9,8 @@ fun main(args: Array<String>) {
 //  removeHtml()
 //  removeURLs()
 //	removePunctuation()
-	removeStopWordsAndOneLetterWords()
+//	removeStopWordsAndOneLetterWords()
+//	stem()
 }
 
 fun getReadFile(fileName: String): File = File(fileName)
@@ -74,7 +75,29 @@ fun removeStopWordsAndOneLetterWords() {
 	if (readFile.exists()) {
 		val lines = Files.lines(Paths.get(readFile.toURI()))
 		val newFile = getOrCreateWriteFile("kotlin-posts-5.csv")
-		val stopWords = listOf("a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours	ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves")
+		val stopWords = listOf("a", "about", "above", "after", "again",
+			"against", "all", "am", "an", "and", "any", "are", "aren't", "as",
+			"at", "be", "because", "been", "before", "being", "below",
+			"between", "both", "but", "by", "can't", "cannot", "could",
+			"couldn't", "did", "didn't", "do", "does", "doesn't", "doing",
+			"don't", "down", "during", "each", "few", "for", "from", "further",
+			"had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he",
+			"he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself",
+			"him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm",
+			"i've", "if", "in", "into", "is", "isn't", "it", "it's", "its",
+			"itself", "let's", "me", "more", "most", "mustn't", "my", "myself",
+			"no", "nor", "not", "of", "off", "on", "once", "only", "or",
+			"other", "ought", "our", "ours	ourselves", "out", "over", "own",
+			"same", "shan't", "she", "she'd", "she'll", "she's", "should",
+			"shouldn't", "so", "some", "such", "than", "that", "that's", "the",
+			"their", "theirs", "them", "themselves", "then", "there", "there's",
+			"these", "they", "they'd", "they'll", "they're", "they've", "this",
+			"those", "through", "to", "too", "under", "until", "up", "very",
+			"was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were",
+			"weren't", "what", "what's", "when", "when's", "where", "where's",
+			"which", "while", "who", "who's", "whom", "why", "why's", "with",
+			"won't", "would", "wouldn't", "you", "you'd", "you'll", "you're",
+			"you've", "your", "yours", "yourself", "yourselves")
 
 		lines.forEach {
 			var text = it.toLowerCase()
@@ -90,5 +113,15 @@ fun removeStopWordsAndOneLetterWords() {
 
 			newFile.appendText("$newLine\n")
 		}
+	}
+}
+
+fun stem() {
+	val readFile = getReadFile("kotlin-posts-5.csv")
+
+	if (readFile.exists()) {
+
+		val output = getOrCreateWriteFile("kotlin-posts-6.csv")
+		Stemmer.stem(readFile.absolutePath, output.absolutePath)
 	}
 }
